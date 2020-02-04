a22962880f0c92e1648fd98fb5328fa2343a10bf
Melusinee <44269357+Melusinee@users.noreply.github.com>
2020-02-04 20:44:04 +0800

    Create Food.java
    Intialized simple food object and related methods

---
+++ Food.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Food {
+	public static final int WIDTH = SnakeFrame.WIDTH;
+	public static final int LENGTH = SnakeFrame.LENGTH;
+	
+	public SnakeFrame snakeFrame;
+	public int col;
+	public int row;
+	
+	public Food(int col,int row) {
+		this.col = col;
+		this.row = row;
+	}
+	
+	public void draw(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(col*WIDTH, row*LENGTH, WIDTH, LENGTH);
+		g.setColor(c);
+	}
+	
+	public void randomAppear() {
+		Random randomRow = new Random();
+		Random randomCol = new Random();
+		
+		this.col = randomCol.nextInt(SnakeFrame.ROW);
+		this.row = randomRow.nextInt(SnakeFrame.COLUMNS);
+	}
+}
